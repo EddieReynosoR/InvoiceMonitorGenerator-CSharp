ca34b6840cddeb40527402372807c5a438be2667
Pablo Herrera <105743191+Pablenki@users.noreply.github.com>
2023-04-29 22:06:05 -0700

    kemishjoto

--- Proyecto Base de Datos/Form1.Designer.cs
+++ Proyecto Base de Datos/Form1.Designer.cs
@@ -47,10 +47,9 @@
             this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
             this.HomeTimer = new System.Windows.Forms.Timer(this.components);
             this.panel5 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.sidebar.SuspendLayout();
             this.panel6.SuspendLayout();
             this.gunaPanel1.SuspendLayout();
@@ -60,9 +59,9 @@
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // sidebar
@@ -75,18 +74,20 @@
             this.sidebar.Controls.Add(this.panel3);
             this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidebar.Location = new System.Drawing.Point(0, 0);
-            this.sidebar.MaximumSize = new System.Drawing.Size(244, 590);
-            this.sidebar.MinimumSize = new System.Drawing.Size(63, 590);
+            this.sidebar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sidebar.MaximumSize = new System.Drawing.Size(274, 738);
+            this.sidebar.MinimumSize = new System.Drawing.Size(71, 738);
             this.sidebar.Name = "sidebar";
-            this.sidebar.Size = new System.Drawing.Size(244, 590);
+            this.sidebar.Size = new System.Drawing.Size(274, 738);
             this.sidebar.TabIndex = 0;
             // 
             // panel6
             // 
             this.panel6.Controls.Add(this.label2);
-            this.panel6.Location = new System.Drawing.Point(3, 3);
+            this.panel6.Location = new System.Drawing.Point(3, 4);
+            this.panel6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(241, 41);
+            this.panel6.Size = new System.Drawing.Size(271, 51);
             this.panel6.TabIndex = 0;
             // 
             // label2
@@ -94,9 +95,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(65, 6);
+            this.label2.Location = new System.Drawing.Point(73, 8);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.Size = new System.Drawing.Size(108, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "Registek";
             // 
@@ -105,17 +106,18 @@
             this.gunaPanel1.Controls.Add(this.label1);
             this.gunaPanel1.Controls.Add(this.menuButton);
             this.gunaPanel1.ForeColor = System.Drawing.Color.White;
-            this.gunaPanel1.Location = new System.Drawing.Point(3, 50);
+            this.gunaPanel1.Location = new System.Drawing.Point(3, 63);
+            this.gunaPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gunaPanel1.Name = "gunaPanel1";
-            this.gunaPanel1.Size = new System.Drawing.Size(241, 61);
+            this.gunaPanel1.Size = new System.Drawing.Size(271, 76);
             this.gunaPanel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 21);
+            this.label1.Location = new System.Drawing.Point(75, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.Size = new System.Drawing.Size(49, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Menu";
             // 
@@ -127,9 +129,10 @@
             this.menuButton.IconChar = FontAwesome.Sharp.IconChar.AlignJustify;
             this.menuButton.IconColor = System.Drawing.Color.White;
             this.menuButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.menuButton.Location = new System.Drawing.Point(-3, 3);
+            this.menuButton.Location = new System.Drawing.Point(-3, 4);
+            this.menuButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.menuButton.Name = "menuButton";
-            this.menuButton.Size = new System.Drawing.Size(64, 52);
+            this.menuButton.Size = new System.Drawing.Size(72, 65);
             this.menuButton.TabIndex = 0;
             this.menuButton.UseVisualStyleBackColor = true;
             this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
@@ -139,19 +142,21 @@
             this.HomeContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(55)))), ((int)(((byte)(60)))));
             this.HomeContainer.Controls.Add(this.panel4);
             this.HomeContainer.Controls.Add(this.panel1);
-            this.HomeContainer.Location = new System.Drawing.Point(3, 117);
-            this.HomeContainer.MaximumSize = new System.Drawing.Size(241, 117);
-            this.HomeContainer.MinimumSize = new System.Drawing.Size(241, 53);
+            this.HomeContainer.Location = new System.Drawing.Point(3, 147);
+            this.HomeContainer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.HomeContainer.MaximumSize = new System.Drawing.Size(271, 146);
+            this.HomeContainer.MinimumSize = new System.Drawing.Size(271, 66);
             this.HomeContainer.Name = "HomeContainer";
-            this.HomeContainer.Size = new System.Drawing.Size(241, 53);
+            this.HomeContainer.Size = new System.Drawing.Size(271, 66);
             this.HomeContainer.TabIndex = 4;
             // 
             // panel4
             // 
             this.panel4.Controls.Add(this.iconButton1);
-            this.panel4.Location = new System.Drawing.Point(0, 68);
+            this.panel4.Location = new System.Drawing.Point(0, 85);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(241, 59);
+            this.panel4.Size = new System.Drawing.Size(271, 74);
             this.panel4.TabIndex = 2;
             // 
             // iconButton1
@@ -163,9 +168,10 @@
             this.iconButton1.IconColor = System.Drawing.Color.White;
             this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton1.Location = new System.Drawing.Point(-3, -18);
+            this.iconButton1.Location = new System.Drawing.Point(-3, -22);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(255, 85);
+            this.iconButton1.Size = new System.Drawing.Size(287, 106);
             this.iconButton1.TabIndex = 1;
             this.iconButton1.Text = "             Submenu";
             this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -174,9 +180,10 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.buttonHome);
-            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Location = new System.Drawing.Point(0, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(241, 59);
+            this.panel1.Size = new System.Drawing.Size(271, 74);
             this.panel1.TabIndex = 1;
             // 
             // buttonHome
@@ -189,9 +196,10 @@
             this.buttonHome.IconColor = System.Drawing.Color.White;
             this.buttonHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.buttonHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonHome.Location = new System.Drawing.Point(-3, -18);
+            this.buttonHome.Location = new System.Drawing.Point(-3, -22);
+            this.buttonHome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonHome.Name = "buttonHome";
-            this.buttonHome.Size = new System.Drawing.Size(255, 85);
+            this.buttonHome.Size = new System.Drawing.Size(287, 106);
             this.buttonHome.TabIndex = 1;
             this.buttonHome.Text = "             Home\r\n";
             this.buttonHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -201,9 +209,10 @@
             // panel2
             // 
             this.panel2.Controls.Add(this.iconButton3);
-            this.panel2.Location = new System.Drawing.Point(3, 176);
+            this.panel2.Location = new System.Drawing.Point(3, 221);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(268, 59);
+            this.panel2.Size = new System.Drawing.Size(302, 74);
             this.panel2.TabIndex = 2;
             // 
             // iconButton3
@@ -215,9 +224,10 @@
             this.iconButton3.IconColor = System.Drawing.Color.White;
             this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton3.Location = new System.Drawing.Point(-3, -17);
+            this.iconButton3.Location = new System.Drawing.Point(-3, -21);
+            this.iconButton3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.iconButton3.Name = "iconButton3";
-            this.iconButton3.Size = new System.Drawing.Size(255, 85);
+            this.iconButton3.Size = new System.Drawing.Size(287, 106);
             this.iconButton3.TabIndex = 1;
             this.iconButton3.Text = "             Gestion de recibos";
             this.iconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -226,9 +236,10 @@
             // panel3
             // 
             this.panel3.Controls.Add(this.iconButton4);
-            this.panel3.Location = new System.Drawing.Point(3, 241);
+            this.panel3.Location = new System.Drawing.Point(3, 303);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(268, 59);
+            this.panel3.Size = new System.Drawing.Size(302, 74);
             this.panel3.TabIndex = 3;
             // 
             // iconButton4
@@ -240,9 +251,10 @@
             this.iconButton4.IconColor = System.Drawing.Color.White;
             this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton4.Location = new System.Drawing.Point(-3, -15);
+            this.iconButton4.Location = new System.Drawing.Point(-3, -19);
+            this.iconButton4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.iconButton4.Name = "iconButton4";
-            this.iconButton4.Size = new System.Drawing.Size(255, 85);
+            this.iconButton4.Size = new System.Drawing.Size(287, 106);
             this.iconButton4.TabIndex = 1;
             this.iconButton4.Text = "             Añadir recibo\r\n";
             this.iconButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -265,67 +277,53 @@
             this.panel5.Controls.Add(this.pictureBox1);
             this.panel5.Controls.Add(this.pictureBox2);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel5.Location = new System.Drawing.Point(244, 0);
+            this.panel5.Location = new System.Drawing.Point(274, 0);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(862, 44);
+            this.panel5.Size = new System.Drawing.Size(970, 55);
             this.panel5.TabIndex = 5;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Proyecto_Base_de_Datos.Properties.Resources.icons8_círculo_rellenado_16__2_1;
-            this.pictureBox2.Location = new System.Drawing.Point(778, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(17, 29);
-            this.pictureBox2.TabIndex = 6;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Proyecto_Base_de_Datos.Properties.Resources.icons8_círculo_rellenado_16__2_1;
-            this.pictureBox1.Location = new System.Drawing.Point(743, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(17, 29);
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Proyecto_Base_de_Datos.Properties.Resources.icons8_círculo_rellenado_16__2_1;
-            this.pictureBox3.Location = new System.Drawing.Point(812, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(914, 15);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(17, 29);
+            this.pictureBox3.Size = new System.Drawing.Size(19, 36);
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
             // 
-            // iconButton2
+            // pictureBox1
             // 
-            this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
-            this.iconButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.iconButton2.ForeColor = System.Drawing.Color.White;
-            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.HomeUser;
-            this.iconButton2.IconColor = System.Drawing.Color.White;
-            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton2.Location = new System.Drawing.Point(502, 159);
-            this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(255, 85);
-            this.iconButton2.TabIndex = 2;
-            this.iconButton2.Text = "             Home\r\n";
-            this.iconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton2.UseVisualStyleBackColor = false;
+            this.pictureBox1.Image = global::Proyecto_Base_de_Datos.Properties.Resources.icons8_círculo_rellenado_16__2_1;
+            this.pictureBox1.Location = new System.Drawing.Point(836, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 36);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Proyecto_Base_de_Datos.Properties.Resources.icons8_círculo_rellenado_16__2_1;
+            this.pictureBox2.Location = new System.Drawing.Point(875, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(19, 36);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1106, 590);
-            this.Controls.Add(this.iconButton2);
+            this.ClientSize = new System.Drawing.Size(1244, 738);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.sidebar);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -340,9 +338,9 @@
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -370,7 +368,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private FontAwesome.Sharp.IconButton iconButton2;
     }
 }
 
